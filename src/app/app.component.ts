9c384f9212002f842683be1697ba9c782a247815
david <davidyluni.personal@gmail.com>
2026-02-22 17:35:17 +0100

    ✨ Implement database initialization in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { DatabaseService } from '@core/services/database.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   imports: [IonApp, IonRouterOutlet],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  private databaseService = inject(DatabaseService);
+
+  async ngOnInit() {
+    // TODO: Implement loading screen while database is being initialized
+    await this.databaseService.init();
+  }
 }
