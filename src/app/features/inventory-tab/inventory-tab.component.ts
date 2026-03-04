faa4ee229fa2ca65bd03c4c362038c757c5c725d
José Escalera <pescalera30@gmail.com>
2026-03-04 11:15:02 +0100

    💄 Implement initial application UI, including tab navigation, home, collection, and inventory pages, global styles, and necessary assets.

--- src/app/features/inventory-tab/inventory-tab.component.ts
+++ src/app/features/inventory-tab/inventory-tab.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IonContent } from '@ionic/angular/standalone';
+import { TitleSignComponent } from '@shared/components/title-sign/title-sign.component';
 
 @Component({
   selector: 'app-inventory-tab',
   templateUrl: 'inventory-tab.component.html',
   styleUrls: ['inventory-tab.component.scss'],
-  imports: [IonContent],
+  imports: [IonContent, TitleSignComponent],
 })
 export class InventoryTabComponent {}
