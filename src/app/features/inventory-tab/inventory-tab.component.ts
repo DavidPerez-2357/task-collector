77977e55e89ca1b4442063045cd8661756e6865b
david <davidyluni.personal@gmail.com>
2026-03-10 12:14:36 +0100

    ✨ Implement inventory tab with infinite scroll and item modal for detailed view

--- src/app/features/inventory-tab/inventory-tab.component.ts
+++ src/app/features/inventory-tab/inventory-tab.component.ts
@@ -1,11 +1,121 @@
-import { Component } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { Component, inject } from '@angular/core';
+import {
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  ViewWillEnter,
+} from '@ionic/angular/standalone';
 import { TitleSignComponent } from '@shared/components/title-sign/title-sign.component';
+import { ShelfComponent } from '@features/inventory-tab/components/shelf/shelf.component';
+import { ItemInventory } from '@core/models/item.model';
+import { ItemService } from '@features/inventory-tab/services/item.service';
+import { InventoryItemModalComponent } from '@features/inventory-tab/components/inventory-item-modal/inventory-item-modal.component';
 
 @Component({
   selector: 'app-inventory-tab',
   templateUrl: 'inventory-tab.component.html',
   styleUrls: ['inventory-tab.component.scss'],
-  imports: [IonContent, TitleSignComponent],
+  imports: [
+    IonContent,
+    TitleSignComponent,
+    ShelfComponent,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    InventoryItemModalComponent,
+  ],
 })
-export class InventoryTabComponent {}
+export class InventoryTabComponent implements ViewWillEnter {
+  protected readonly ITEMS_PER_SHELF = 3;
+  protected readonly MIN_SHELVES = 3;
+  protected readonly SHELVES_PER_PAGE = 5;
+  protected readonly PAGE_SIZE = this.ITEMS_PER_SHELF * this.SHELVES_PER_PAGE;
+
+  private readonly itemService = inject(ItemService);
+
+  actualPage: number = 1;
+  allItems: ItemInventory[] = [];
+  itemsShelves: ItemInventory[][] = [];
+
+  // Modal
+  selectedItem: ItemInventory | null = null;
+  isModalOpen: boolean = false;
+
+  async ionViewWillEnter() {
+    this.allItems = [];
+    this.itemsShelves = [];
+    this.actualPage = 1;
+
+    // Poner el scroll al principio
+    await this.scrollToTop();
+
+    await this.loadMoreItems(this.actualPage);
+
+    if (this.itemsShelves.length < this.MIN_SHELVES) {
+      const emptyShelvesNeeded = this.MIN_SHELVES - this.itemsShelves.length;
+      for (let i = 0; i < emptyShelvesNeeded; i++) {
+        this.itemsShelves.push([]);
+      }
+    }
+  }
+
+  async scrollToTop() {
+    const content = document.querySelector('ion-content');
+    if (content) {
+      await content.scrollToTop(0);
+    }
+  }
+
+  async loadMoreItems(page: number): Promise<void> {
+    try {
+      const newItems = await this.itemService.getInventoryItemsPaginated(page, this.PAGE_SIZE);
+      this.allItems = [...this.allItems, ...newItems];
+      this.pushPageItemsToShelves(page);
+    } catch (error) {
+      console.error('Error loading items:', error);
+      // TODO: Mostrar un mensaje de error al usuario
+    }
+  }
+
+  pushPageItemsToShelves(page: number) {
+    const startIndex = (page - 1) * this.PAGE_SIZE;
+    const endIndex = startIndex + this.PAGE_SIZE;
+    const pageItems = this.allItems.slice(startIndex, endIndex);
+
+    for (let i = 0; i < pageItems.length; i++) {
+      const shelfIndex = Math.floor((startIndex + i) / this.ITEMS_PER_SHELF);
+      const item = pageItems[i];
+
+      if (!this.itemsShelves[shelfIndex]) {
+        this.itemsShelves[shelfIndex] = [];
+      }
+
+      if (item === undefined) {
+        console.warn(`Item at index ${startIndex + i} is undefined. Skipping.`);
+        continue;
+      }
+
+      this.itemsShelves[shelfIndex].push(item);
+    }
+  }
+
+  protected async onIonInfinite($event: any) {
+    if (this.allItems.length < this.PAGE_SIZE * this.actualPage) {
+      $event.target.complete();
+      return;
+    }
+
+    this.actualPage++;
+    await this.loadMoreItems(this.actualPage);
+    $event.target.complete();
+  }
+
+  itemClicked(item: ItemInventory) {
+    this.selectedItem = item;
+    this.isModalOpen = true;
+  }
+
+  onModalDismissed() {
+    this.isModalOpen = false;
+    this.selectedItem = null;
+  }
+}
